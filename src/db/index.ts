1d702a655d32ccdd4115e444fdd7c1c515be8c73
Ali Alnaghmoush <alialnaghmoush@gmail.com>
2025-05-08 15:53:39 +0300

    v0.1.11
    - Updated various dependencies to their latest versions, including:
      - @hookform/resolvers, @neondatabase/serverless, @radix-ui/react-* packages, better-auth, drizzle-orm, intlayer, lucide-react, next, next-intlayer, react, react-dom, react-hook-form, tailwind-merge, zod, eslint-config-next, tw-animate-css
    - Modified db connection setup in src/db/index.ts to include commented-out code for using read replicas with drizzle-orm

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,20 @@
 import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/neon-serverless';
+// import { withReplicas } from 'drizzle-orm/pg-core';
 
 config({ path: '.env.local' });
+
+// ================================================
+// if you want to use the primary db only
+// ================================================
 const db = drizzle({ connection: process.env.DATABASE_URL!, casing: 'snake_case' });
+
+// ================================================
+// if you want to use the primary and replica db
+// ================================================
+// const primaryDb = drizzle({ connection: process.env.DATABASE_URL!, casing: 'snake_case' });
+// const readReplicaDb1 = drizzle(process.env.DATABASE_URL_RR1!);
+
+// const db = withReplicas(primaryDb, [readReplicaDb1]);
 
 export default db;
